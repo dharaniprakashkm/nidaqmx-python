7c5f5cc9a3d1c81552f5bb93e1d54fc3ee2dcac9
Praneeth Bhogaraju <praneeth.bhogaraju@ni.com>
2024-06-27 20:11:50 +0530

    Annother one

--- generated/nidaqmx/_linux_installation_commands.py
+++ generated/nidaqmx/_linux_installation_commands.py
@@ -55,6 +55,9 @@ _YUM_INSTALL_COMMANDS = [
     ["dkms", "autoinstall"],
 ]
 
+_DEBIAN_DAQMX_VERSION_COMMAND = ["dpkg", "-l", "ni-daqmx"]
+_RPM_DAQMX_VERSION_COMMAND = ["rpm", "-q", "ni-daqmx"]
+
 @dataclass
 class DistroInfo:
     get_distro_version: Callable[[str], str]
